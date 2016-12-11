93be1777a9f0e2868bdf2b0b9488fd72eb58159f
dmanliclic715 <dmanliclic715@gmail.com>
2016-12-10 20:17:13 -0600

    Add AA Vendor controller

--- app/admin/vendor.rb
+++ app/admin/vendor.rb
@@ -12,6 +12,17 @@ ActiveAdmin.register Vendor do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+  permit_params :als_account_number, :vendor_name, :billing_address, :billing_city, :billing_state, :billing_zip, :shipping_address, :shipping_city, :shipping_state, :shipping_zip, :po_number, :notes, :contact_person, :sales_person, :mark_up, :per_hour_rate, :tax_rate, :terms
 
+
+  sidebar :number, only: :show do
+    table_for vendor.phone_numbers do
+      column "Type"
+      column "Number" do |blah|
+        blah.number
+      end
+      column :notes
+    end
+  end
 
 end
