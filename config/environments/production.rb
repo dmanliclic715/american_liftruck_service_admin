e5c1fcad92161438f7c308ac10649707ac0df055
dmanliclic715 <dmanliclic715@gmail.com>
2016-12-10 20:21:32 -0600

    Try to configure time setting to allow for american time formatting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  Date::DATE_FORMATS[:default] = '%m/%d/%Y'
+  Time::DATE_FORMATS[:default]= '%m/%d/%Y %H:%M:%S'
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
