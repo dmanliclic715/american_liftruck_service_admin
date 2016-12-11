d77195cd06bdeec66278a4351d50926693a90adf
dmanliclic715 <dmanliclic715@gmail.com>
2016-12-10 20:23:45 -0600

    Add customers migration

--- db/migrate/20161201044230_create_customers.rb
+++ db/migrate/20161201044230_create_customers.rb
@@ -1,24 +1,25 @@
 class CreateCustomers < ActiveRecord::Migration[5.0]
   def change
-    create_table :customers do |t|
-      t.string :name, null: false
-      t.string :billing_address, null: false
-      t.string :billing_city, null: false
-      t.string :billing_state, null: false
-      t.string :billing_zip, null: false
-      t.string :shipping_address, null: false
-      t.string :shipping_city, null: false
-      t.string :shipping_state, null: false
-      t.string :shipping_zip, null: false
+    create_table :customers, id: false do |t|
+      t.primary_key :id
+      t.string :name
+      t.string :billing_address
+      t.string :billing_city
+      t.string :billing_state
+      t.string :billing_zip
+      t.string :shipping_address
+      t.string :shipping_city
+      t.string :shipping_state
+      t.string :shipping_zip
       t.string :po_number
       t.text :notes
       t.string :contact_person
       t.string :sales_person
       t.integer :mark_up
       t.integer :per_hour_rate
-      t.integer :tax_rate
+      t.float :tax_rate
       t.integer :terms
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
